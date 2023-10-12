e209fc90875489a7b090e217d136cf56bce38971
Gilthayllor Sousa <gilthayllor@outlook.com>
2023-10-12 11:31:34 -0300

    Adição da ViewModel para criação de Book

---
+++ Application/ViewModel/CreateBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AspNetUnityOfWork.Application.ViewModel
+{
+    public class CreateBookViewModel
+    {
+        public string Name { get; set; }
+        public int AuthorId { get; set; }
+
+        public CreateBookViewModel(string name, int authorId)
+        {
+            Name = name;
+            AuthorId = authorId;
+        }
+    }
+}
